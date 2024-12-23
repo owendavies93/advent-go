d8569c57957435fe1a2b9cb25243a8f85c020057
Owen Davies <owen@odavi.es>
2024-12-23 09:30:51 +0000

    Search up directories for example inputs - useful for unit tests

--- util/input.go
+++ util/input.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
 func GetInput(year int, day int) (string, error) {
@@ -19,13 +20,19 @@ func GetInput(year int, day int) (string, error) {
 }
 
 func GetExampleInput(year int, day int) (string, error) {
-	file := fmt.Sprintf("inputs/%d/%02d-test", year, day)
-
-	if _, err := os.Stat(file); err == nil {
-		return file, nil
-	} else if errors.Is(err, os.ErrNotExist) {
-		return "", fmt.Errorf("example input does not exist")
-	} else {
-		return "", fmt.Errorf("error getting example input: %w", err)
+	currentDir, err := os.Getwd()
+	if err != nil {
+		return "", fmt.Errorf("error getting current directory: %w", err)
 	}
+
+	for dir := currentDir; dir != "/"; dir = filepath.Dir(dir) {
+		file := filepath.Join(dir, fmt.Sprintf("inputs/%d/%02d-test", year, day))
+		if _, err := os.Stat(file); err == nil {
+			return file, nil
+		} else if !errors.Is(err, os.ErrNotExist) {
+			return "", fmt.Errorf("error checking file: %w", err)
+		}
+	}
+
+	return "", fmt.Errorf("example input does not exist in any parent directory")
 }
